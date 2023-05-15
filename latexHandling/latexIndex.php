37f4fc59fd6203a46a7349711cddd6ba9be72246
Simona <xzlatohlavkova@stuba.sk>
2023-05-15 22:38:01 +0200

    dizaj students questions

--- latexHandling/latexIndex.php
+++ latexHandling/latexIndex.php
@@ -10,10 +10,83 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>LaTeX file upload</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+              integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/98d99917c7.js" crossorigin="anonymous"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="../css/menu.css" rel="stylesheet"/>
+        <link href="../css/styles.css" rel="stylesheet"/>
+        <link href="../latexHandling/php/test.css" rel="stylesheet"/>
     </head>
     <body>
-        <div class="container">
+
+    <header id="nav-wrapper">
+        <nav id="nav">
+            <div class="nav left">
+                <span class="gradient skew"><h1 class="logo un-skew"><a id="logoID">Školský portál  </a></h1></span>
+                <button id="menu" class="btn-nav"><span class="fas fa-bars"></span></button>
+            </div>
+            </div>
+            <div class="nav right">
+                <a href="/controllers/logout-controller.php" class="nav-link"><span class="nav-link-span active"><span class="u-nav">Odhlásenie</span></span></a>
+            </div>
+        </nav>
+    </header>
+
+    <input id="toggle" type="checkbox">
+
+    <label for="toggle" class="hamburger">
+        <div class="top-bun"></div>
+        <div class="meat"></div>
+        <div class="bottom-bun"></div>
+    </label>
+
+    <div class="navSmall">
+        <div class="navSmall-wrapperSmall">
+            <nav id="navSmallHref">
+                <a href="/controllers/logout-controller.php">Odhlásiť sa</a><br>
+            </nav>
+        </div>
+    </div>
+
+    <div class="container-fluid" id="fileUploadFlex" >
+
+            <div class="card card-upload">
+                <div class="card-body">
+                        <h1>Nahrajte LaTex súbor</h1>
+                        <div class="rectangle">
+                            <div class="rectangle-content">
+                                <form>
+                                    <label for="file-input">
+                                        <input  class="form-control" id="file-input" type="file" name="latexFile" accept=".tex" multiple="false">
+                                    </label>
+                                    <button id="upload-button"  class="btn btn-primary" type="button"> Nahrať</button>
+                                </form>
+                            </div>
+                        </div>
+                </div>
+            </div>
+            <div class="card card-upload" >
+                <div class="card-body">
+
+                    <h1>Nahrajte obrázok pre LaTexový súbor </h1>
+                            <form>
+                                <label for="image-input">
+                                    <input class="form-control" id="image-input" type="file" name="latexImage" multiple="false">
+                                </label>
+                                <button id="upload-image-button" class="btn btn-primary" type="button"> Nahrať</button>
+                            </form>
+
+                </div>
+            </div>
+        </div>
+
+
+
+    <!---    <div class="container">
             <h1>Upload LaTeX file here</h1>
             <div class="rectangle">
                 <div class="rectangle-content">
@@ -38,7 +111,14 @@
                     </form>
                 </div>
             </div>
-        </div>
+        </div>-->
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
+            integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
+            integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ"
+            crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
         <script src="js/upload.js"></script>
     </body>
--- latexHandling/php/studentQuestions.php
+++ latexHandling/php/studentQuestions.php
@@ -151,81 +151,81 @@ if (isset($_SESSION['latexFile'])) {
     <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+          integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/98d99917c7.js" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="../../css/menu.css" rel="stylesheet" />
-    <link href="../../css/styles.css" rel="stylesheet" />
-    <link href="test.css" rel="stylesheet" />
+    <link href="../../css/menu.css" rel="stylesheet"/>
+    <link href="../../css/styles.css" rel="stylesheet"/>
+    <link href="test.css" rel="stylesheet"/>
+    <link href="tabsStyle.css" rel="stylesheet"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
 </head>
 
 <body>
 
-    <header id="nav-wrapper">
-        <nav id="nav">
-            <div class="nav left">
+<header id="nav-wrapper">
+    <nav id="nav">
+        <div class="nav left">
                 <span class="gradient skew">
                     <h1 class="logo un-skew"><a id="logoID">Školský portál </a></h1>
                 </span>
-                <button id="menu" class="btn-nav"><span class="fas fa-bars"></span></button>
-            </div>
-            <div class="nav right">
-                <a href="../../index.php" class="nav-link"><span class="nav-link-span active"><span
-                            class="u-nav">Prihlásenie</span></span></a>
-            </div>
-
-        </nav>
-    </header>
-
-    <input id="toggle" type="checkbox">
-
-    <label for="toggle" class="hamburger">
-        <div class="top-bun"></div>
-        <div class="meat"></div>
-        <div class="bottom-bun"></div>
-    </label>
-
-    <div class="navSmall">
-        <div class="navSmall-wrapperSmall">
-            <nav id="navSmallHref">
-                <a href="../../index.php">Prihlásenie</a><br>
-            </nav>
+            <button id="menu" class="btn-nav"><span class="fas fa-bars"></span></button>
         </div>
+        <div class="nav right">
+            <a href="../../controllers/logout-controller.php" class="nav-link"><span class="nav-link-span active"><span
+                            class="u-nav">Odhlásenie</span></span></a>
+        </div>
+
+    </nav>
+</header>
+
+<input id="toggle" type="checkbox">
+
+<label for="toggle" class="hamburger">
+    <div class="top-bun"></div>
+    <div class="meat"></div>
+    <div class="bottom-bun"></div>
+</label>
+
+<div class="navSmall">
+    <div class="navSmall-wrapperSmall">
+        <nav id="navSmallHref">
+            <a href="../../index.php">Prihlásenie</a><br>
+        </nav>
     </div>
+</div>
 
 
-    <div class="container-fluid centered-container " id="questionsFlex">
+<div class="wholePage" >
+    <div class="card">
+        <div class="card-body">
+            <div>
+                <form method="POST" action="select-file.php">
+                    <label for="file-name">Vyberte súbor:</label>
+                    <select name="file-name" id="file-name" class="form-select">
+                        <option value="" disabled selected>Vyberte príklad</option>
+                        <?php
+                        foreach ($names as $name) {
+                            echo '<option value="' . $name['name'] . '">' . $name['name'] . '</option>';
+                        }
+                        ?>
 
-        <div class="card">
-            <div class="card-body">
-
-                <div>
-                    <form method="POST" action="select-file.php">
-                        <label for="file-name">Vyberte súbor:</label>
-                        <select name="file-name" id="file-name" class="form-select">
-                            <option value="" disabled selected>Vyberte príklad</option>
-                            <?php
-                            foreach ($names as $name) {
-                                echo '<option value="' . $name['name'] . '">' . $name['name'] . '</option>';
-                            }
-                            ?>
-
-                        </select>
-                        <button type="submit" class="btn btn-success">Generovať príklad</button>
-                    </form>
-                </div>
-
-                <div>
-                    <?php
-                    if ($questionExists) {
-                        echo "Tento príklad už bol vygenerovaný";
-                    }
-                    ?>
-                </div>
-
+                    </select>
+                    <button type="submit" class="btn btn-primary">Generovať príklad</button>
+                </form>
             </div>
 
-            <div class="card-body">
+            <div>
+                <?php
+                if ($questionExists) {
+                    echo "Tento príklad už bol vygenerovaný";
+                }
+                ?>
+            </div>
+            <div>
                 <h5 class="card-title">Príklad
                     <?php
                     if (empty($questions)) {
@@ -235,6 +235,9 @@ if (isset($_SESSION['latexFile'])) {
                     }
                     ?>
                 </h5>
+            </div>
+
+            <div>
                 <?php
 
                 if (empty($questions)) {
@@ -259,15 +262,46 @@ if (isset($_SESSION['latexFile'])) {
                 }
 
                 ?>
-
             </div>
         </div>
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Prehľad príkladov</h5>
-                <?php
-                foreach ($studentQuestions as $studentQuestion) {
-                    echo "<div class='questionName'>" . $studentQuestion['question_name'] . "</div>";
+    </div>
+    <div class="card" id="questionsCard">
+        <div class="card-body">
+            <h5 class="card-title">Príklady</h5>
+
+            <ul class="nav nav-tabs nav-questions" id="myTab" role="tablist">
+                <?php $first = true;
+                $i = 0; ?>
+                <?php foreach ($studentQuestions as $studentQuestion) { ?>
+                    <li class="nav-item" style="width: auto">
+                        <a class="nav-link"
+                           style="color: inherit; transform: skew(0deg); background: inherit; font-size: x-small"
+                           id="<?php echo $i; ?>-tab" data-toggle="tab" href="#<?php echo $i; ?>"
+                           role="tab"
+
+                           style="font-size: x-small">
+                            <?php echo $studentQuestion['question_name']; ?></a>
+                    </li>
+                    <?php $first = false;
+                    $i++; ?>
+                <?php } ?>
+            </ul>
+
+
+                <div class="tab-content" id="myTabContent" style="margin-top: 6rem;">
+                    <?php
+                    $first = true;
+                    $i = 0;
+                    foreach ($studentQuestions
+
+                    as $studentQuestion) {
+                    ?>
+                    <div class="tab-pane" <?php if ($i === 0) {
+                        echo "active";
+                    } else {
+                        echo "";
+                    } ?>" id="<?php echo $i; ?>" role="tabpanel" >
+                    <div class='questionName'><?php $studentQuestion['question_name']; ?></div> <?php
                     echo "<div id='question' class='question'>" . $studentQuestion['question'] . "</div>";
 
                     $sqlSelectImage = "SELECT image FROM latexImages WHERE name = ?";
@@ -278,24 +312,45 @@ if (isset($_SESSION['latexFile'])) {
                     if ($imageDB) {
                         $imageData = base64_encode($imageDB['image']);
                         $image = "data:image/png;base64," . $imageData;
-                        echo '<div class="image"><img width="auto" src="' . $image . '" alt="' . $image . '"></div>';
+                        echo '<div class="image"><img width="auto" height="auto" src="' . $image . '" alt="' . $image . '"></div>';
                     }
 
 
                     echo '<a href="submitMath.php?sectionName=' . urlencode($studentQuestion['question_name']) . '">Odovzdať</a>';
 
-                    echo "<hr>";
+                    echo "<hr>"; ?>
+                </div><?php
+                $i++;
+                $first = false;
                 }
                 ?>
             </div>
-        </div>
 
     </div>
+</div>
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
+</div>
+
+
+<!--<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="..." alt="First slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="..." alt="Second slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="..." alt="Third slide">
+        </div>
+    </div>
+</div>-->
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
         integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE"
         crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
         integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ"
         crossorigin="anonymous"></script>
 </body>
