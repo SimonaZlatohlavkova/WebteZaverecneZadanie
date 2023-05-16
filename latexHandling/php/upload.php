86ff4a6cfa210031344f6765e9fb70f5486079da
adamk <123Kuci321>
2023-05-16 19:05:08 +0200

    pridanie bodov a datumov k zadaniu

--- latexHandling/php/upload.php
+++ latexHandling/php/upload.php
@@ -12,10 +12,24 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 if (isset($_FILES["latexFile"])) {
     $file = $_FILES["latexFile"]["tmp_name"];
     $fileData = file_get_contents($file);
+    $points = $_POST['points'];
+    $dateFrom = $_POST['dateFrom'];
+    $dateTo = $_POST['dateTo'];
 
-    $sql = "INSERT INTO latexFiles (name, latexFile) VALUES (?, ?)";
-    $stmt = $db->prepare($sql);
-    $success = $stmt->execute([$_FILES["latexFile"]["name"], $fileData]);
+    if($dateFrom !== "" && $dateTo !== "") {
+
+        $sql = "INSERT INTO latexFiles (name, latexFile, points, validFrom, validTo) VALUES (?, ?, ?, ?, ?)";
+        $stmt = $db->prepare($sql);
+        $success = $stmt->execute([$_FILES["latexFile"]["name"], $fileData, $points, $dateFrom, $dateTo]);
+    }
+
+    else{
+
+        $sql = "INSERT INTO latexFiles (name, latexFile, points) VALUES (?, ?, ?)";
+        $stmt = $db->prepare($sql);
+        $success = $stmt->execute([$_FILES["latexFile"]["name"], $fileData, $points]);
+    }
+
 
     if ($success) {
         echo "File uploaded successfully!";
@@ -27,10 +41,21 @@ if (isset($_FILES["latexFile"])) {
 if (isset($_FILES["image"])){
     $file = $_FILES["image"]["tmp_name"];
     $fileData = file_get_contents($file);
+    $points = $_POST['points'];
+    $dateFrom = $_POST['dateFrom'];
+    $dateTo = $_POST['dateTo'];
 
-    $sql = "INSERT INTO latexImages (name, image) VALUES (?, ?)";
-    $stmt = $db->prepare($sql);
-    $success = $stmt->execute([$_FILES["image"]["name"], $fileData]);
+    if($dateFrom !== "" && $dateTo !== "") {
+
+        $sql = "INSERT INTO latexImages (name, image, points, validFrom, validTo) VALUES (?, ?, ?, ?, ?)";
+        $stmt = $db->prepare($sql);
+        $success = $stmt->execute([$_FILES["image"]["name"], $fileData, $points, $dateFrom, $dateTo]);
+    }
+    else{
+        $sql = "INSERT INTO latexImages (name, image) VALUES (?, ?)";
+        $stmt = $db->prepare($sql);
+        $success = $stmt->execute([$_FILES["image"]["name"], $fileData]);
+    }
 
     if ($success) {
         echo "Image uploaded successfully!";
