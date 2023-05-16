583de94506ea0a16520b294b52cf44f1db7cc532
Simona <xzlatohlavkova@stuba.sk>
2023-05-16 13:03:38 +0200

    oprava head

--- menu/teacherMenu.php
+++ menu/teacherMenu.php
@@ -25,6 +25,14 @@ if ($_SESSION["role"] == "student") {
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>School Portal</title>
     <link href="../css/styles.css" type="text/css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>School Portal</title>
+    <link href="../css/styles.css" rel="stylesheet"/>
+    <link href="../css/menu.css" rel="stylesheet"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/98d99917c7.js" crossorigin="anonymous"></script>
+    <link href="teacherMenu.css" rel="stylesheet" />
 </head>
 <body>
 
@@ -41,14 +49,6 @@ if ($_SESSION["role"] == "student") {
             <a href="../latexHandling/latexIndex.php" class="nav-link"><span class="nav-link-span active"><span
                             class="u-nav">Príklady</span></span></a>
         </div>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>School Portal</title>
-        <link href="../css/styles.css" rel="stylesheet"/>
-        <link href="../css/menu.css" rel="stylesheet"/>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-              integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-        <script src="https://kit.fontawesome.com/98d99917c7.js" crossorigin="anonymous"></script>
-        <link href="teacherMenu.css" rel="stylesheet" />
     </nav>
 </header>
 
