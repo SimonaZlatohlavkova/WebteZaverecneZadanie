6095d6fdc10b1bc05c6a1cfff36d2129d51bf212
antoniokis <xkisa@stuba.sk>
2023-05-16 22:26:04 +0200

    prehlad studentov - detail

--- details/studentDetail.php
+++ details/studentDetail.php
@@ -76,14 +76,38 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 </div>
                 <div class="nav right">
                     <a href="../details/studentDetail.php" class="nav-link"><span class="nav-link-span active"><span
-                                class="u-nav">Prehľad študentov</span></span></a>
+                                    class="u-nav">Prehľad študentov</span></span></a>
                     <a href="../controllers/logout-controller.php" class="nav-link"><span class="nav-link-span active"><span
-                                class="u-nav">Odhlásenie</span></span></a>
+                                    class="u-nav">Odhlásenie</span></span></a>
                     <a href="../latexHandling/latexIndex.php" class="nav-link"><span class="nav-link-span active"><span
-                                class="u-nav">Príklady</span></span></a>
+                                    class="u-nav">Príklady</span></span></a>
                 </div>
             </nav>
         </header>
+
+        <input id="toggle" type="checkbox">
+
+        <label for="toggle" class="hamburger">
+            <div class="top-bun"></div>
+            <div class="meat"></div>
+            <div class="bottom-bun"></div>
+        </label>
+
+        <div class="navSmall">
+            <div class="navSmall-wrapperSmall">
+                <nav id="navSmallHref">
+                    <a href="../details/studentDetail.php">Prehľad študentov</a><br>
+                </nav>
+                <nav id="navSmallHref">
+                    <a href="../controllers/logout-controller.php">Odhlásenie</a><br>
+                </nav>
+                <nav id="navSmallHref">
+                    <a href="../latexHandling/latexIndex.php">Príklady</a><br>
+                </nav>
+            </div>
+        </div>
+
+
 
 
         <div class="container">
@@ -118,7 +142,12 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
             </table>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
+                integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE"
+                crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
+                integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ"
+                crossorigin="anonymous"></script>
         <script src="../scripts/tables.js"></script>
     </body>
 </html>
