7fa26d2bfc03544874bc0baf4d0c05ddfad64fb3
antoniokis <xkisa@stuba.sk>
2023-05-16 17:56:14 +0200

    pristupove prava

--- details/studentDetail.php
+++ details/studentDetail.php
@@ -3,7 +3,7 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 if (!isset($_SESSION["login"]) || !$_SESSION["login"]) {
-    header("Location: ../registration.php");
+    header("Location: ../index.php");
     exit();
 }
 
