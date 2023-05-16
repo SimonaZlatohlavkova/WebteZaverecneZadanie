ef6003fd95bd3d29c4d1e064e7ad78f94e8e64f3
Simona <xzlatohlavkova@stuba.sk>
2023-05-16 20:18:51 +0200

    multilanguage latexIndex.php

--- latexHandling/js/upload.js
+++ latexHandling/js/upload.js
@@ -5,22 +5,49 @@ uploadButton.addEventListener('click', () => {
     const pointsFile = document.getElementById('pointsFile').value;
     const dateFromFile = document.getElementById('dateFromFile').value;
     const dateToFile = document.getElementById('dateToFile').value;
-
+    var valid=false;
     if (!fileInput) {
+        document.getElementById('file-input').style.borderColor = "red";
         console.log('No file selected');
-        return;
-    }
-    const formData = new FormData();
-    formData.append('latexFile', fileInput);
-    formData.append('points', pointsFile);
-    formData.append('dateFrom', dateFromFile);
-    formData.append('dateTo', dateToFile);
+        valid=false;
 
-    axios.post('php/upload.php', formData)
-        .then((response) => {
-            console.log(response.data);
-        })
-        .catch((error) => console.log(error));
+    } else {
+        document.getElementById('file-input').style.borderColor = "inherit";
+        valid=true;
+    }
+    console.log(pointsFile)
+    if (!pointsFile || pointsFile==="") {
+        document.getElementById('pointsFile').style.borderColor = "red";
+        console.log('No points selected');
+        valid=false;
+
+    } else {
+        var pattern = /^\d*\.?\d+$|^\d+$/;
+
+        if (pattern.test(pointsFile)) {
+            console.log("Value matches the pattern.");
+            document.getElementById('pointsFile').style.borderColor = "inherit";
+            valid=true;
+        } else {
+            document.getElementById('pointsFile').style.borderColor = "red";
+            console.log("Value does not match the pattern.");
+            valid=false;
+        }
+
+    }
+    if (valid===true) {
+        const formData = new FormData();
+        formData.append('latexFile', fileInput);
+        formData.append('points', pointsFile);
+        formData.append('dateFrom', dateFromFile);
+        formData.append('dateTo', dateToFile);
+
+        axios.post('php/upload.php', formData)
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => console.log(error));
+    }
 });
 
 const uploadImageButton = document.getElementById('upload-image-button');
